3c440c2f391927e2f3661ccf1c4bf675b32b2ce1
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-11-12 10:03:12 -0600

    First Module Angular

---
+++ src/app/Contadores/contador.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ContadorComponent } from './Contador/contador.component';
+
+@NgModule({
+  declarations: [ContadorComponent],
+  exports: [ContadorComponent],
+  imports: [],
+})
+export class ContadorModule {}
