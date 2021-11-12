5ddb595ecb515f3d4463a5676c774c6328eb55af
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-11-12 13:49:15 -0600

    Add services and Forms

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css'],
+})
+export class AgregarComponent {
+  @Input() nuevo: Personaje = { nombre: '', poder: 0 };
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  constructor(private dbzService: DbzService) {}
+
+  agregar() {
+    if (this.nuevo.nombre === '') return;
+    this.dbzService.agregarPersonaje(this.nuevo);
+    // this.onNuevoPersonaje.emit(this.nuevo);
+    this.nuevo = { nombre: '', poder: 0 };
+  }
+}
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css'],
+})
+export class MainPageComponent {
+  nuevo: Personaje = { nombre: 'Maestro Roshi', poder: 1000 };
+
+  // get personajes(): Personaje[] {
+  //   return this.dbzService.personajes;
+  // }
+
+  // agregarNuevoPersonaje(arg: Personaje) {
+  //   this.personajes.push(arg);
+  // }
+
+  //constructor(private dbzService: DbzService) {}
+  constructor() {}
+}
